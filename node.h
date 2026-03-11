35fc3942bead9d59ba5c0dbebc2581ca05716576
moumen-h <moumen.prs@gmail.com>
2026-03-11 14:57:04 +0100

    Create header file for typedefs and function prototypes

---
+++ node.h
@@ -0,0 +1,32 @@
+#ifndef NODE_H
+#define NODE_H
+
+typedef enum Status {
+  SUCCESS = 0,
+  FAILURE = 1,
+} Status;
+
+typedef struct Node {
+  struct Node *next;
+  int value;
+} Node;
+
+Node *create(int value);
+Status destroy(Node *head);
+
+Status append(Node *head, int value);
+Status prepend(Node **head, int value);
+Status insertAt(Node *head, int index, int value);
+
+int removeFirst(Node **head);
+int removeLast(Node *head);
+int removeAt(Node *head, int index);
+
+int getLength(Node *head);
+int getFirst(Node *head);
+int getLast(Node *head);
+int at(Node *head, int index);
+
+int findFirst(Node *head, int value);
+
+#endif
